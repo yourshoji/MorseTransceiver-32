f80c941b227d140b3ce0618627aba0f91db570a4
yourshoji <shojihub@gmail.com>
2026-08-01 14:33:56 +0700

    TIM4 and ADC conversion completed

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,10 +21,13 @@
 #include "adc.h"
 #include "gpio.h"
 #include "i2c.h"
+#include "stm32f103xb.h"
 #include "tim.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
@@ -48,7 +51,6 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-extern TIM_HandleTypeDef htim4;
 
 /// @brief Holds dot lengths in milliseconds that can change while the program
 /// runs.
@@ -150,8 +152,6 @@ int main(void) {
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM4_Init();
-  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
   ssd1306_Init();
   play_intro_ui();
@@ -160,7 +160,7 @@ int main(void) {
    * The timer speed depends on user settings (unit_presets).
    * This setup gives a 10 kHz interrupt rate at 72 MHz. */
 
-  /**
+  /*
    * @brief TIM2 setup
    */
   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
@@ -173,7 +173,7 @@ int main(void) {
   NVIC_EnableIRQ(TIM2_IRQn);
   TIM2->CR1 |= TIM_CR1_CEN;
 
-  /**
+  /*
    * @brief TIM3 setup
    */
   RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
@@ -184,15 +184,40 @@ int main(void) {
   TIM3->SMCR |= (0x3 << 0);
   TIM3->CCMR1 = (1 << 0) | (0xA << 4) | (1 << 8) | (0xA << 12);
   TIM3->CCER &= ~(TIM_CCER_CC1P | TIM_CCER_CC2P);
-  TIM3->CCER &= ~(TIM_CCER_CC1P | TIM_CCER_CC2P);
-  TIM3->PSC = 0;
+  TIM3->CCER |= (TIM_CCER_CC1E | TIM_CCER_CC2E);
   TIM3->PSC = 0;
   TIM3->ARR = 107;
   TIM3->CR1 |= TIM_CR1_CEN;
 
-  HAL_TIM_PWM_Start(LED2_TIM, LED2_CHANNEL);
+  /*
+   *  @brief TIM4 & PWM setup
+   */
+  RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+  GPIOB->CRL &= ~(0xFUL << 24);
+  GPIOB->CRL |= (0xBUL << 24);
+  TIM4->CCMR1 |= (0x6 << 4) | (1 << TIM_CCMR1_OC1PE);
+  TIM4->CCER &= ~TIM_CCER_CC1P;
+  TIM4->CCER |= TIM_CCER_CC1E;
+  TIM4->CCR1 = 0;
+  TIM4->PSC = 0;
+  TIM4->ARR = 103;
+  TIM4->CR1 |= TIM_CR1_CEN;
 
-  HAL_ADCEx_Calibration_Start(&hadc1);
+  /*
+   *  @brief ADC setup
+   */
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+  GPIOA->CRL &= ~(0xFUL << 4);
+  ADC1->SQR3 = 1;
+  ADC1->SMPR2 &= ~(0x7UL << 3);
+  ADC1->SMPR2 |= (0x0UL << 3);
+  ADC1->CR2 |= ADC_CR2_ADON;
+  delay_ms(1);
+  ADC1->CR2 |= ADC_CR2_CAL;
+  while (ADC1->CR2 & ADC_CR2_CAL) {
+  }
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -262,9 +287,7 @@ int main(void) {
 /* USER CODE END WHILE */
 
 /* USER CODE BEGIN 3 */
-}
 /* USER CODE END 3 */
-}
 
 /**
  * @brief System Clock Configuration
@@ -590,13 +613,10 @@ bool handle_transmit(uint16_t pulse_duration) {
 void handle_ldr_receive(uint32_t threshold, uint32_t current_pwm_level) {
   reset_and_tune_handler();
 
-  HAL_ADC_Start(&hadc1);
-  if (HAL_ADC_PollForConversion(&hadc1, 10) != HAL_OK) {
-    HAL_ADC_Stop(&hadc1);
-    return;
+  ADC1->CR2 |= ADC_CR2_SWSTART;
+  while (!(ADC1->SR & ADC_SR_EOC)) {
   }
-
-  rx.ldr_val = HAL_ADC_GetValue(&hadc1);
+  rx.ldr_val = ADC1->DR;
   uint32_t now = millis();
 
   if (rx.ldr_val > threshold) {
@@ -605,7 +625,7 @@ void handle_ldr_receive(uint32_t threshold, uint32_t current_pwm_level) {
       rx.is_light_on = true;
     }
 
-    __HAL_TIM_SET_COMPARE(LED2_TIM, LED2_CHANNEL, 1000);
+    TIM4->CCR1 = 1000;
     BUZZER_PORT->BSRR = BUZZER_PIN;
     LED3_PORT->BSRR = LED3_PIN;
 
@@ -626,7 +646,7 @@ void handle_ldr_receive(uint32_t threshold, uint32_t current_pwm_level) {
       rx.gap_start = now;
     }
 
-    __HAL_TIM_SET_COMPARE(LED2_TIM, LED2_CHANNEL, current_pwm_level);
+    TIM4->CCR1 = current_pwm_level;
     BUZZER_PORT->BSRR = (BUZZER_PIN << 16);
     LED3_PORT->BSRR = (LED3_PIN << 16);
 
@@ -658,8 +678,6 @@ void handle_ldr_receive(uint32_t threshold, uint32_t current_pwm_level) {
       }
     }
   }
-
-  HAL_ADC_Stop(&hadc1);
 }
 
 /// @brief Checks how long the button is held to clear text or change speed.
